e30b190e78ca0a4455ed35f000a0270779cc256c
manel <manelzaum@icloud.com>
2026-05-16 18:56:57 -0300

    fix: prevent much langs in statusline with multiple :so.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,9 +17,14 @@ vim.o.scrolloff = 20
 
 vim.g.c_syntax_for_h = 1 -- Detect .h as c, not cpp
 
-vim.opt.laststatus = 3  -- Transforma a statusline em uma barra única e global
+vim.opt.laststatus = 3   -- Set statusline to unique and global
 
-vim.opt.statusline:append(" %{v:lua.get_buffer_lang()}")
+local status_addon = " %{v:lua.get_buffer_lang()}"
+local current_status = vim.opt.statusline:get()
+
+if not string.find(current_status, "get_buffer_lang", 1, true) then
+  vim.opt.statusline:append(status_addon)
+end
 
 function get_buffer_lang()
     local ft = vim.bo.filetype
@@ -44,7 +49,7 @@ vim.cmd([[
 ]])
 
 -- Keymaps
-vim.keymap.set({ "n" }, "<Esc>", "<cmd>nohlsearch<CR>")
+keymap.set({ "n" }, "<Esc>", "<cmd>nohlsearch<CR>")
 
 ----------------------------------------------------------------------------------------
 --- Plugins
@@ -53,28 +58,28 @@ vim.keymap.set({ "n" }, "<Esc>", "<cmd>nohlsearch<CR>")
 local pack = vim.pack
 
 -- Catppuccin | Colorscheme
-pack.add { { name = "catppuccin", src = "https://github.com/catppuccin/nvim" } }
+pack.add { { src = "https://github.com/catppuccin/nvim" } }
 vim.cmd.colorscheme "catppuccin-nvim"
 
 -- opencode.nvim | AI code assistant
-pack.add { { name = "opencode.nvim", src = "https://github.com/nickjvandyke/opencode.nvim" } }
+pack.add { { src = "https://github.com/nickjvandyke/opencode.nvim" } }
 
 vim.g.opencode_opts = {}
 
 vim.o.autoread = true
 
-vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
-vim.keymap.set({ "n", "x" }, "<leader>ox", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
-vim.keymap.set({ "n", "t" }, "<leader>ot", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
+keymap.set({ "n", "x" }, "<leader>ox", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
+keymap.set({ "n", "t" }, "<leader>ot", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
 
-vim.keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
-vim.keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
+keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
+keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
 
-vim.keymap.set("n", "<C-U>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
-vim.keymap.set("n", "<C-D>", function() require("opencode").command("session.half.page.down") end,   { desc = "Scroll opencode down" })
+keymap.set("n", "<C-U>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
+keymap.set("n", "<C-D>", function() require("opencode").command("session.half.page.down") end,   { desc = "Scroll opencode down" })
 
 -- Neogit | Git interface
-pack.add { { name = "neogit", src = "https://github.com/NeogitOrg/neogit" } }
+pack.add { { src = "https://github.com/NeogitOrg/neogit" } }
 
 require("neogit").setup {
   disable_line_numbers = true,
@@ -96,29 +101,29 @@ require("neogit").setup {
   },
 }
 
-vim.keymap.set("n", "<leader>gg",
+keymap.set("n", "<leader>gg",
     function() require("neogit").open({ kind = "floating" }) end,
     { desc = "Open Neogit" })
 
 -- nvim-tmux-navigation | Navigate between nvim and tmux panes
-pack.add { { name = "nvim-tmux-navigation", src = "https://github.com/alexghergh/nvim-tmux-navigation" } }
+pack.add { { src = "https://github.com/alexghergh/nvim-tmux-navigation" } }
 
 local nvim_tmux_nav = require("nvim-tmux-navigation")
 nvim_tmux_nav.setup {
   disable_when_zoomed = true,
 }
 
-vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
 
 -- scrollBeforeEOF | Keep scrolloff at bottom of file
-pack.add { { name = "scrollBeforeEOF", src = "https://github.com/emdrs/scrollBeforeEOF" } }
+pack.add { { src = "https://github.com/emdrs/scrollBeforeEOF" } }
 require("scrollBeforeEOF").setup()
 
 -- oil.nvim | File explorer
-pack.add { { name = "oil.nvim", src = "https://github.com/stevearc/oil.nvim" } }
+pack.add { { src = "https://github.com/stevearc/oil.nvim" } }
 require("oil").setup {
     default_file_explorer = true,
     delete_to_trash = true,
@@ -132,27 +137,27 @@ require("oil").setup {
     }
 }
 
-vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open oil file explorer" })
+keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open oil file explorer" })
 
 -- multiple-cursors.nvim | Multiple cursors
-pack.add { { name = "multiple-cursors.nvim", src = "https://github.com/brenton-leighton/multiple-cursors.nvim" } }
+pack.add { { src = "https://github.com/brenton-leighton/multiple-cursors.nvim" } }
 
 require("multiple-cursors").setup {
     remove_in_opposite_direction = false,
 }
 
-vim.keymap.set("n", "<A-Up>",    "<cmd>MultipleCursorsAddUp<CR>",            { desc = "Add cursor up" })
-vim.keymap.set("n", "<A-Down>",  "<cmd>MultipleCursorsAddDown<CR>",          { desc = "Add cursor down" })
-vim.keymap.set("n", "<leader>a", "<cmd>MultipleCursorsAddMatches<CR>",       { desc = "Select all matches" })
-vim.keymap.set("n", "<leader>n", "<cmd>MultipleCursorsAddJumpNextMatch<CR>", { desc = "Add cursor at next match" })
-vim.keymap.set("n", "<leader>p", "<cmd>MultipleCursorsAddJumpPrevMatch<CR>", { desc = "Add cursor at previous match" })
+keymap.set("n", "<A-Up>",    "<cmd>MultipleCursorsAddUp<CR>",            { desc = "Add cursor up" })
+keymap.set("n", "<A-Down>",  "<cmd>MultipleCursorsAddDown<CR>",          { desc = "Add cursor down" })
+keymap.set("n", "<leader>a", "<cmd>MultipleCursorsAddMatches<CR>",       { desc = "Select all matches" })
+keymap.set("n", "<leader>n", "<cmd>MultipleCursorsAddJumpNextMatch<CR>", { desc = "Add cursor at next match" })
+keymap.set("n", "<leader>p", "<cmd>MultipleCursorsAddJumpPrevMatch<CR>", { desc = "Add cursor at previous match" })
 
 -- plenary.nvim | Dependency for telescope
-pack.add { { name = "plenary.nvim", src = "https://github.com/nvim-lua/plenary.nvim" } }
+pack.add { { src = "https://github.com/nvim-lua/plenary.nvim" } }
 
 -- telescope.nvim | Fuzzy finder
-pack.add { { name = "telescope.nvim", src = "https://github.com/nvim-telescope/telescope.nvim" } }
-pack.add { { name = "telescope-fzf-native.nvim", src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" } }
+pack.add { { src = "https://github.com/nvim-telescope/telescope.nvim" } }
+pack.add { { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" } }
 
 local telescope = require("telescope")
 telescope.setup {
@@ -184,15 +189,15 @@ telescope.setup {
 
 telescope.load_extension("fzf")
 
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
-vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>",  { desc = "Live grep" })
-vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>",    { desc = "Buffers" })
-vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>",  { desc = "Help tags" })
-vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>",   { desc = "Recent files" })
-vim.keymap.set("n", "<leader>fc", "<cmd>Telescope commands<CR>",   { desc = "Commands" })
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
+keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>",  { desc = "Live grep" })
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>",    { desc = "Buffers" })
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>",  { desc = "Help tags" })
+keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>",   { desc = "Recent files" })
+keymap.set("n", "<leader>fc", "<cmd>Telescope commands<CR>",   { desc = "Commands" })
 
 -- vimtex | LaTeX editing
-pack.add { { name = "vimtex", src = "https://github.com/lervag/vimtex" } }
+pack.add { { src = "https://github.com/lervag/vimtex" } }
 
 vim.g.vimtex_view_method = "skim"
 vim.g.vimtex_compiler_method = "latexmk"
@@ -209,16 +214,18 @@ vim.g.vimtex_compiler_latexmk = {
 }
 
 -- compile-mode.nvim | Run compilation commands
-pack.add { { name = "compile-mode.nvim", src = "https://github.com/ej-shafran/compile-mode.nvim" } }
+pack.add { { src = "https://github.com/ej-shafran/compile-mode.nvim" } }
 
 vim.g.compile_mode = {
     default_command = "",
     focus_compilation_buffer = true,
 }
 
-vim.keymap.set("n", "<leader>cc", "<cmd>Compile<CR>", { desc = "Run compile command" })
+keymap.set("n", "<leader>cc", "<cmd>Compile<CR>", { desc = "Run compile command" })
 
 -- tree-sitter-manager.nvim | Tree-sitter installation manager
-pack.add { { name = "tree-sitter-manager.nvim", src = "https://github.com/romus204/tree-sitter-manager.nvim" } }
+pack.add { { src = "https://github.com/romus204/tree-sitter-manager.nvim" } }
 
 require("tree-sitter-manager").setup()
+
+keymap.set("n", "<leader>tsm", "<cmd>TSManager<CR>", { desc = "Show TSManager" })
